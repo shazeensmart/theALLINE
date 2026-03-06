8ea14e84c24099c695efad2b6bd6c14d3616f1d2
shazeensmart <shazeensmart@gmail.com>
2026-03-07 03:27:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let cart = [];
+
+function addToCart(product){
+
+cart.push(product);
+
+displayCart();
+
+}
+
+function displayCart(){
+
+let cartList = document.getElementById("cart-items");
+
+cartList.innerHTML="";
+
+cart.forEach(item => {
+
+let li = document.createElement("li");
+
+li.textContent = item;
+
+cartList.appendChild(li);
+
+});
+
+}
